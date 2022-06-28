fba4bb654a902e1cbee2346c50fa269ecb8eb20d
Mauri <maurijunk02@gmail.com>
2022-06-28 20:36:29 -0300

    asd

---
+++ Form1.Designer.cs
@@ -0,0 +1,325 @@
+﻿namespace Presentacion
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnMostrar = new System.Windows.Forms.Button();
+            this.txtDNIAct = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dgvMuestra = new System.Windows.Forms.DataGridView();
+            this.nOMBREDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.aPELLIDODataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.gENERODataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dNIDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.uSUARIOSBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.proyectoDataSet = new Presentacion.ProyectoDataSet();
+            this.txtDNI = new System.Windows.Forms.TextBox();
+            this.txtGenero = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnModificar = new System.Windows.Forms.Button();
+            this.btnInsertar = new System.Windows.Forms.Button();
+            this.uSUARIOSTableAdapter = new Presentacion.ProyectoDataSetTableAdapters.USUARIOSTableAdapter();
+            this.uSUARIOSBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMuestra)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uSUARIOSBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.proyectoDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uSUARIOSBindingSource1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnMostrar
+            // 
+            this.btnMostrar.Location = new System.Drawing.Point(430, 119);
+            this.btnMostrar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnMostrar.Name = "btnMostrar";
+            this.btnMostrar.Size = new System.Drawing.Size(100, 28);
+            this.btnMostrar.TabIndex = 30;
+            this.btnMostrar.Text = "Mostrar";
+            this.btnMostrar.UseVisualStyleBackColor = true;
+            this.btnMostrar.Click += new System.EventHandler(this.btnMostrar_Click);
+            // 
+            // txtDNIAct
+            // 
+            this.txtDNIAct.Location = new System.Drawing.Point(766, 55);
+            this.txtDNIAct.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDNIAct.Name = "txtDNIAct";
+            this.txtDNIAct.Size = new System.Drawing.Size(132, 22);
+            this.txtDNIAct.TabIndex = 29;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(762, 24);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(131, 16);
+            this.label5.TabIndex = 28;
+            this.label5.Text = "DNI A ACTUALIZAR:";
+            // 
+            // dgvMuestra
+            // 
+            this.dgvMuestra.AllowUserToDeleteRows = false;
+            this.dgvMuestra.AutoGenerateColumns = false;
+            this.dgvMuestra.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvMuestra.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nOMBREDataGridViewTextBoxColumn,
+            this.aPELLIDODataGridViewTextBoxColumn,
+            this.gENERODataGridViewTextBoxColumn,
+            this.dNIDataGridViewTextBoxColumn});
+            this.dgvMuestra.DataSource = this.uSUARIOSBindingSource1;
+            this.dgvMuestra.Location = new System.Drawing.Point(42, 191);
+            this.dgvMuestra.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvMuestra.Name = "dgvMuestra";
+            this.dgvMuestra.ReadOnly = true;
+            this.dgvMuestra.RowHeadersWidth = 51;
+            this.dgvMuestra.Size = new System.Drawing.Size(818, 498);
+            this.dgvMuestra.TabIndex = 27;
+            this.dgvMuestra.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvMuestra_CellContentClick);
+            // 
+            // nOMBREDataGridViewTextBoxColumn
+            // 
+            this.nOMBREDataGridViewTextBoxColumn.DataPropertyName = "NOMBRE";
+            this.nOMBREDataGridViewTextBoxColumn.HeaderText = "NOMBRE";
+            this.nOMBREDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.nOMBREDataGridViewTextBoxColumn.Name = "nOMBREDataGridViewTextBoxColumn";
+            this.nOMBREDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // aPELLIDODataGridViewTextBoxColumn
+            // 
+            this.aPELLIDODataGridViewTextBoxColumn.DataPropertyName = "APELLIDO";
+            this.aPELLIDODataGridViewTextBoxColumn.HeaderText = "APELLIDO";
+            this.aPELLIDODataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.aPELLIDODataGridViewTextBoxColumn.Name = "aPELLIDODataGridViewTextBoxColumn";
+            this.aPELLIDODataGridViewTextBoxColumn.Width = 125;
+            // 
+            // gENERODataGridViewTextBoxColumn
+            // 
+            this.gENERODataGridViewTextBoxColumn.DataPropertyName = "GENERO";
+            this.gENERODataGridViewTextBoxColumn.HeaderText = "GENERO";
+            this.gENERODataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.gENERODataGridViewTextBoxColumn.Name = "gENERODataGridViewTextBoxColumn";
+            this.gENERODataGridViewTextBoxColumn.Width = 125;
+            // 
+            // dNIDataGridViewTextBoxColumn
+            // 
+            this.dNIDataGridViewTextBoxColumn.DataPropertyName = "DNI";
+            this.dNIDataGridViewTextBoxColumn.HeaderText = "DNI";
+            this.dNIDataGridViewTextBoxColumn.MinimumWidth = 6;
+            this.dNIDataGridViewTextBoxColumn.Name = "dNIDataGridViewTextBoxColumn";
+            this.dNIDataGridViewTextBoxColumn.Width = 125;
+            // 
+            // uSUARIOSBindingSource
+            // 
+            this.uSUARIOSBindingSource.DataMember = "USUARIOS";
+            this.uSUARIOSBindingSource.DataSource = this.proyectoDataSet;
+            // 
+            // proyectoDataSet
+            // 
+            this.proyectoDataSet.DataSetName = "ProyectoDataSet";
+            this.proyectoDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // txtDNI
+            // 
+            this.txtDNI.Location = new System.Drawing.Point(532, 55);
+            this.txtDNI.Margin = new System.Windows.Forms.Padding(4);
+            this.txtDNI.Name = "txtDNI";
+            this.txtDNI.Size = new System.Drawing.Size(132, 22);
+            this.txtDNI.TabIndex = 26;
+            // 
+            // txtGenero
+            // 
+            this.txtGenero.Location = new System.Drawing.Point(367, 55);
+            this.txtGenero.Margin = new System.Windows.Forms.Padding(4);
+            this.txtGenero.Name = "txtGenero";
+            this.txtGenero.Size = new System.Drawing.Size(132, 22);
+            this.txtGenero.TabIndex = 25;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(202, 55);
+            this.txtApellido.Margin = new System.Windows.Forms.Padding(4);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(132, 22);
+            this.txtApellido.TabIndex = 24;
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(32, 55);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(132, 22);
+            this.txtName.TabIndex = 23;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(528, 24);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(33, 16);
+            this.label4.TabIndex = 22;
+            this.label4.Text = "DNI:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(363, 24);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 16);
+            this.label3.TabIndex = 21;
+            this.label3.Text = "Genero:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(198, 24);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(60, 16);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Apellido:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 24);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 19;
+            this.label1.Text = "Nombre:";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Location = new System.Drawing.Point(296, 119);
+            this.btnEliminar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(100, 28);
+            this.btnEliminar.TabIndex = 18;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Location = new System.Drawing.Point(170, 119);
+            this.btnModificar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(100, 28);
+            this.btnModificar.TabIndex = 17;
+            this.btnModificar.Text = "Modificar";
+            this.btnModificar.UseVisualStyleBackColor = true;
+            this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
+            // 
+            // btnInsertar
+            // 
+            this.btnInsertar.Location = new System.Drawing.Point(42, 119);
+            this.btnInsertar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnInsertar.Name = "btnInsertar";
+            this.btnInsertar.Size = new System.Drawing.Size(100, 28);
+            this.btnInsertar.TabIndex = 16;
+            this.btnInsertar.Text = "Insertar";
+            this.btnInsertar.UseVisualStyleBackColor = true;
+            this.btnInsertar.Click += new System.EventHandler(this.btnInsertar_Click);
+            // 
+            // uSUARIOSTableAdapter
+            // 
+            this.uSUARIOSTableAdapter.ClearBeforeFill = true;
+            // 
+            // uSUARIOSBindingSource1
+            // 
+            this.uSUARIOSBindingSource1.DataMember = "USUARIOS";
+            this.uSUARIOSBindingSource1.DataSource = this.proyectoDataSet;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(975, 699);
+            this.Controls.Add(this.btnMostrar);
+            this.Controls.Add(this.txtDNIAct);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dgvMuestra);
+            this.Controls.Add(this.txtDNI);
+            this.Controls.Add(this.txtGenero);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnModificar);
+            this.Controls.Add(this.btnInsertar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvMuestra)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uSUARIOSBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.proyectoDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.uSUARIOSBindingSource1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnMostrar;
+        private System.Windows.Forms.TextBox txtDNIAct;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView dgvMuestra;
+        private System.Windows.Forms.TextBox txtDNI;
+        private System.Windows.Forms.TextBox txtGenero;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnModificar;
+        private System.Windows.Forms.Button btnInsertar;
+        private ProyectoDataSet proyectoDataSet;
+        private System.Windows.Forms.BindingSource uSUARIOSBindingSource;
+        private ProyectoDataSetTableAdapters.USUARIOSTableAdapter uSUARIOSTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nOMBREDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn aPELLIDODataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn gENERODataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dNIDataGridViewTextBoxColumn;
+        private System.Windows.Forms.BindingSource uSUARIOSBindingSource1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Data.SqlClient;
+
+namespace Presentacion
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        string conexion = "Data Source=LAPTOP-MAURI;Initial Catalog=Proyecto;Integrated Security=True";
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: esta línea de código carga datos en la tabla 'proyectoDataSet.USUARIOS' Puede moverla o quitarla según sea necesario.
+            this.uSUARIOSTableAdapter.Fill(this.proyectoDataSet.USUARIOS);
+
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            using (SqlConnection cn = new SqlConnection(conexion))
+            {
+                string query = "insert into USUARIOS values ('" + txtName.Text + "','" + txtApellido.Text + "','" + txtGenero.Text + "'," + txtDNI.Text + ")";
+                SqlCommand cmd = new SqlCommand(query, cn);
+                cmd.CommandType = CommandType.Text; // Indica el tipo de comando
+
+                cn.Open();
+                cmd.ExecuteNonQuery();  // Ejecuta COmando
+
+                mostrar();
+
+            }
+        }
+
+        private void btnModificar_Click(object sender, EventArgs e)
+        {
+
+            using (SqlConnection cn = new SqlConnection(conexion))
+            {
+                string query = "update USUARIOS set NOMBRE = '" + txtName.Text + "' , APELLIDO = '" + txtApellido.Text + "', GENERO = '" + txtGenero.Text + "', DNI = '" + txtDNI.Text + "' where DNI = '" + txtDNIAct.Text + "'";
+                SqlCommand cmd = new SqlCommand(query, cn);
+                cmd.CommandType = CommandType.Text;
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+
+                mostrar();
+
+            }
+
+
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+            using (SqlConnection cn = new SqlConnection(conexion))
+            {
+                string query = "delete from USUARIOS where DNI = '" + txtDNIAct.Text + "'";
+                SqlCommand cmd = new SqlCommand(query, cn);
+                cmd.CommandType = CommandType.Text;
+
+                cn.Open();
+                cmd.ExecuteNonQuery();
+                mostrar();
+
+            }
+
+        }
+
+        private void btnMostrar_Click(object sender, EventArgs e)
+        {
+
+            mostrar();
+
+
+        }
+
+        private void mostrar()
+        {
+            DataTable tabla = new DataTable();  // Representa una tabla en la memoria virtual
+
+            using (SqlConnection cn = new SqlConnection(conexion))
+            {
+                string query = "select * from USUARIOS";
+
+                SqlDataAdapter da = new SqlDataAdapter(query, cn);  // Representa comando y conexion para rellenar la tabla
+
+                da.SelectCommand.CommandType = CommandType.Text;
+
+
+                cn.Open();
+
+                da.Fill(tabla); // Llena la tabla
+
+                dgvMuestra.DataSource = tabla; // Muestra la tabla
+
+
+            }
+        }
+
+        private void dgvMuestra_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
